3d3351a80dc6e7f22693ea159d1de11fd00308c2
Raymond Perminov <raymondproduction@ukr.net>
2017-06-02 00:56:54 +0300

    New system for routs

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,5 @@
  $(document).ready(function() {
 
-   // Функция для вывода виджета по идентификатору
-   // шаблона
-   // в регионе #content
-   function showView(sameTemplate) {
-     // создаем конструктор виджета
-     var SameView = Mn.View.extend({
-       template: sameTemplate
-     });
-
-     // создали виджет
-     var sameView = new SameView();
-
-     // указываем создаем виджет для визуализации
-     // в определенном регионе используя
-     // сконструированный в качестве
-     // дочернего
-     var MyView = Mn.View.extend({
-       el: '#main_section',
-       template: false,
-       regions: {
-         region: '#content',
-       },
-       onRender() {
-         this.showChildView('region', sameView);
-       }
-     });
-     var myView = new MyView();
-     myView.render();
-   }
-
-
-
    // Настраиваем приложение Backbone Marionette
    var App = Mn.Application.extend({
      region: '#content', // указываем регион
@@ -40,19 +8,6 @@
      // Backbone Marionette
      onStart: function() {
 
-       // контроллер для отображения виджетов
-       myController = {
-         showVal: function(val) {
-           showView('#' + val);
-         }
-       };
-       // Создаем роутер (маршрутизатор)
-       var MyRouter = Mn.AppRouter.extend({
-         controller: myController,
-         appRoutes: {
-           ':val': 'showVal'
-         }
-       })
 
 
        var myRouter = new MyRouter();
---
+++ src/js/router.js
@@ -0,0 +1,43 @@
+   // Функция для вывода виджета по идентификатору
+   // шаблона
+   // в регионе #content
+   function showView(sameTemplate) {
+     // создаем конструктор виджета
+     var SameView = Mn.View.extend({
+       template: sameTemplate
+     });
+
+     // создали виджет
+     var sameView = new SameView();
+
+     // указываем создаем виджет для визуализации
+     // в определенном регионе используя
+     // сконструированный в качестве
+     // дочернего
+     var MyView = Mn.View.extend({
+       el: '#main_section',
+       template: false,
+       regions: {
+         region: '#content',
+       },
+       onRender() {
+         this.showChildView('region', sameView);
+       }
+     });
+     var myView = new MyView();
+     myView.render();
+   }
+
+   // контроллер для отображения виджетов
+   myController = {
+     showVal: function(val) {
+       showView('#' + val);
+     }
+   };
+   // Создаем роутер (маршрутизатор)
+   var MyRouter = Mn.AppRouter.extend({
+     controller: myController,
+     appRoutes: {
+       ':val': 'showVal'
+     }
+   })
